98446da4d2a48b8e6bc880ff95588aa4505ec3e7
angeniferrer <cpe.arielangeni.ferrer@gmail.com>
2023-08-25 12:48:38 +0800

    White Blog About

--- Dist/js/script.js
+++ Dist/js/script.js
@@ -1,7 +1,16 @@
 const toggleBtn = document.querySelector(".toggle--button");
 const headerNav = document.querySelector(".header--nav");
+const headerNav2 = document.querySelector(".header--nav2");
+const toggleBtn2 = document.querySelector(".toggle--button2");
 
-toggleBtn.addEventListener("click", () => {
-  toggleBtn.classList.toggle("active");
-  headerNav.classList.toggle("active");
-});
+if (toggleBtn) {
+  toggleBtn.addEventListener("click", () => {
+    toggleBtn.classList.toggle("active");
+    headerNav.classList.toggle("active");
+  });
+} else {
+  toggleBtn2.addEventListener("click", () => {
+    toggleBtn2.classList.toggle("active");
+    headerNav2.classList.toggle("active");
+  });
+}
